3bf990e9e0ba0cfaad002bce9464bd6ed29f95a2
amadorcf <amadorcf@outlook.com>
2024-07-15 12:31:57 +0200

    Initial commit

---
+++ src/test/java/amadorcf/es/YourBank_eureka_server/YourBankEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package amadorcf.es.YourBank_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourBankEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
